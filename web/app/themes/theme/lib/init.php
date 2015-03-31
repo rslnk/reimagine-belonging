c48687aa5bf5e9d22a61a76da2f7eb5f8eb461b6
Ruslan Komjakov <ruslan.komjakov@gmail.com>
2015-03-31 04:53:58 +0200

    Configure init.php

--- web/app/themes/theme/lib/init.php
+++ web/app/themes/theme/lib/init.php
@@ -19,7 +19,8 @@ function setup() {
   // Register wp_nav_menu() menus
   // http://codex.wordpress.org/Function_Reference/register_nav_menus
   register_nav_menus([
-    'primary_navigation' => __('Primary Navigation', 'sage')
+    'primary_navigation'    => __('Primary Navigation', 'sage'),
+    'secondary_navigation'  => __('Secondary Navigation', 'sage')
   ]);
 
   // Add post thumbnails
@@ -28,39 +29,21 @@ function setup() {
   // http://codex.wordpress.org/Function_Reference/add_image_size
   add_theme_support('post-thumbnails');
 
+  // Disable intermediate sizes generation on image upload
+  update_option( 'medium_size_h', 0 );
+  update_option( 'medium_size_w', 0 );
+  update_option( 'large_size_h', 0 );
+  update_option( 'large_size_w', 0 );
+
   // Add post formats
   // http://codex.wordpress.org/Post_Formats
-  add_theme_support('post-formats', ['aside', 'gallery', 'link', 'image', 'quote', 'video', 'audio']);
+  //add_theme_support('post-formats', ['aside', 'gallery', 'link', 'image', 'quote', 'video', 'audio']);
 
   // Add HTML5 markup for captions
   // http://codex.wordpress.org/Function_Reference/add_theme_support#HTML5
   add_theme_support('html5', ['caption', 'comment-form', 'comment-list']);
 
   // Tell the TinyMCE editor to use a custom stylesheet
-  add_editor_style(Assets\asset_path('styles/editor-style.css'));
+  //add_editor_style(Assets\asset_path('styles/editor-style.css'));
 }
 add_action('after_setup_theme', __NAMESPACE__ . '\\setup');
-
-/**
- * Register sidebars
- */
-function widgets_init() {
-  register_sidebar([
-    'name'          => __('Primary', 'sage'),
-    'id'            => 'sidebar-primary',
-    'before_widget' => '<section class="widget %1$s %2$s">',
-    'after_widget'  => '</section>',
-    'before_title'  => '<h3>',
-    'after_title'   => '</h3>'
-  ]);
-
-  register_sidebar([
-    'name'          => __('Footer', 'sage'),
-    'id'            => 'sidebar-footer',
-    'before_widget' => '<section class="widget %1$s %2$s">',
-    'after_widget'  => '</section>',
-    'before_title'  => '<h3>',
-    'after_title'   => '</h3>'
-  ]);
-}
-add_action('widgets_init', __NAMESPACE__ . '\\widgets_init');
