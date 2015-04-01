d08dfc7fe5cf4403333c3f3faa1b0d91b12b290b
Ruslan Komjakov <ruslan.komjakov@gmail.com>
2015-04-01 19:42:25 +0200

    Add save/load directory for ACF PRO custom fields JSON

--- web/app/mu-plugins/base-setup.php
+++ web/app/mu-plugins/base-setup.php
@@ -26,3 +26,29 @@ if ('http://' . $_SERVER['SERVER_NAME'] . '/wp' == get_option('siteurl')) {
   update_option('uploads_use_yearmonth_folders', false);
   update_option('permalink_structure', '/%postname%/');
 }
+
+/*
+  ACF PRO Setup
+
+  Set custom save/load path to JSON custom fields
+  http://www.advancedcustomfields.com/resources/local-json/
+ */
+
+add_filter('acf/settings/save_json', 'custom_acf_json_save_point');
+add_filter('acf/settings/load_json', 'custom_acf_json_load_point');
+
+function custom_acf_json_save_point( $path ) {
+  // update path
+  $path = WPMU_PLUGIN_DIR . '/acf-custom-fields';
+  // return
+  return $path;
+}
+function custom_acf_json_load_point( $paths ) {
+  // remove original path (optional)
+  unset($paths[0]);
+  // append path
+  //$paths[] = get_stylesheet_directory() . '../mu-plugins/acf-custom-fields';
+  $paths[] = WPMU_PLUGIN_DIR .'/acf-custom-fields';
+  // return
+  return $paths;
+}
