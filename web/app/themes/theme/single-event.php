f3db96dbead08952fc53a0139ba0f831a4bb1639
Ruslan Komjakov <ruslan.komjakov@gmail.com>
2015-04-24 16:29:16 +0200

    Add basic templates

---
+++ web/app/themes/theme/single-event.php
@@ -0,0 +1,1 @@
+<?php get_template_part('templates/content-event', get_post_type()); ?>
---
+++ web/app/themes/theme/templates/content-story.php
@@ -0,0 +1,19 @@
+<?php while (have_posts()) : the_post(); ?>
+  <article class="story-post">
+    <header>
+      <div class="story-date">
+        <?= Roots\Sage\Extras\list_custom_taxonomies_terms(); ?>
+      </div>
+      <h1 class="event-title"><?php the_title(); ?></h1>
+      <?php if (get_field('subtitle')): ?>
+        <h2 class="post-subtitle"><?php the_field('subtitle'); ?></h2>
+      <?php endif; ?>
+    </header>
+    <div class="post-content">
+      <?php the_field('main_content'); ?>
+    </div>
+    <footer></footer>
+  </article>
+<?php endwhile; ?>
+
+<?php  ?>
