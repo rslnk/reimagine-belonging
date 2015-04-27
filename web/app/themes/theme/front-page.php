c28b28957b8ff6f576c2a16f4c16f27faf0f4b39
Ruslan Komjakov <ruslan.komjakov@gmail.com>
2015-04-27 22:27:33 +0200

    Update homepage intro image

--- web/app/themes/theme/front-page.php
+++ web/app/themes/theme/front-page.php
@@ -1,9 +1,9 @@
  <div class="u-centering-wrapper">
    <div class="u-centered">
-    <div class="o-wrapper c-home-intro">
+    <div class="o-wrapper">
       <h2 class="o-heading c-heading--home t-negative">We encourage you to join us and together</h2>
       <h1 class="o-site-logo c-site-logo--intro u-icon u-icon-logo-negative">Reimagine Belonging</h1>
     </div>
   </div>
 </div>
-<div class="u-cover" style="background-image: url(<? echo get_template_directory_uri() ?>/assets/images/reimagine-belonging.jpg);"></div>
+<div class="u-cover" style="background-image: url(<? echo get_template_directory_uri() ?>/assets/images/homepage-intro-image--1.0.jpg);"></div>
