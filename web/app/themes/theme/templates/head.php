ef796a482571e39d2c2e3a4f03fa9dbbb825db67
Ruslan Komjakov <ruslan.komjakov@gmail.com>
2015-04-29 01:27:25 +0200

    Add Proxima Nova and Open Sans fonts

--- web/app/themes/theme/templates/head.php
+++ web/app/themes/theme/templates/head.php
@@ -5,6 +5,8 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="alternate" type="application/rss+xml" title="<?= get_bloginfo('name'); ?> Feed" href="<?= esc_url(get_feed_link()); ?>">
+    <link href='http://fonts.googleapis.com/css?family=Open+Sans:300italic,400italic,400,300,600,700' rel='stylesheet' type='text/css'>
+    <link type="text/css" rel="stylesheet" href="http://fast.fonts.net/cssapi/dae2ada1-fb62-4216-ab20-8072b137a586.css"/>
     <link type="text/css" rel="stylesheet" href="<?php echo get_template_directory_uri() ?>/dist/styles/icons.svg.css"/>
     <?php wp_head(); ?>
     <base href="/history/"></base>
