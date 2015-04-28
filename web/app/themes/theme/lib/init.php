81d6ca2fc818ef78a4cb40d572b63f977870a2f9
Ruslan Komjakov <ruslan.komjakov@gmail.com>
2015-04-28 18:08:31 +0200

    Add custom fields to story post type

--- web/app/themes/theme/lib/init.php
+++ web/app/themes/theme/lib/init.php
@@ -39,7 +39,7 @@ function setup() {
 
   // Add post formats
   // http://codex.wordpress.org/Post_Formats
-  add_theme_support('post-formats', ['image', 'video', 'audio']);
+  add_theme_support('post-formats', ['video', 'audio']);
 
   // Add HTML5 markup for captions
   // http://codex.wordpress.org/Function_Reference/add_theme_support#HTML5
