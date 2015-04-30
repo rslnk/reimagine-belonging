ec51ef0b2ce1c10b8f4a7fc67ed21cf2afb7a233
Sergei Sapoznikov <sergei.sapoznikov@gmail.com>
2015-04-30 20:32:14 +0100

    Add wrapper for title

--- web/app/themes/theme/template-timeline.php
+++ web/app/themes/theme/template-timeline.php
@@ -3,9 +3,11 @@
  * Template Name: Timeline
  */
 ?>
-
-<?php the_field('subtitle') ?>
-
+<div class="o-wrapper">
+    <h1 class="o-heading c-heading--page-title">
+        <?php the_field('subtitle') ?>
+    </h1>
+</div>
 <div ng-app="eventsApp">
   <div ui-view></div>
 </div>
