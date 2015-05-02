9185b0f8dc9a69cd56e4f268cfda8799db2b9b73
Ruslan Komjakov <ruslan.komjakov@gmail.com>
2015-05-02 17:43:02 +0200

    Fix main nav toggle icon position on mobile, fixes #37, adjusted other nav element styles

--- web/app/themes/theme/templates/footer.php
+++ web/app/themes/theme/templates/footer.php
@@ -5,12 +5,12 @@
     <nav class="o-site-nav c-site-nav--footer">
       <?php
       if (has_nav_menu('secondary_navigation')) :
-        wp_nav_menu(['theme_location' => 'secondary_navigation', 'walker' => new NavWalker(), 'menu_class' => 'c-site-nav__list _c-site-nav__list--footer']);
+        wp_nav_menu(['theme_location' => 'secondary_navigation', 'walker' => new NavWalker(), 'menu_class' => 'c-site-nav__list _site-nav__list--footer']);
       endif;
       ?>
-      <ul class="c-site-nav__list _c-site-nav__list--footer c-site-nav__list--footer-extra">
-        <li class="li"><a href="#">Sign up</a></li>
-        <li class="li"><a href="#">Sprechen sie Deutsch?</a></li>
+      <ul class="c-site-nav__list _site-nav__list--footer c-site-nav__list--footer-extra">
+        <li><a href="#">Sign up</a></li>
+        <li><a href="#">Sprechen sie Deutsch?</a></li>
       </ul>
     </nav>
   </div>
