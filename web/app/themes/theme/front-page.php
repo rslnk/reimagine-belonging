54cbea5b2094be5a4fd7bec04dd0e72e00556e0d
Ruslan Komjakov <ruslan.komjakov@gmail.com>
2015-05-02 18:53:37 +0200

    Adjust homepage intro elements posistions and sizes. Fixes #39 and closes #40

--- web/app/themes/theme/front-page.php
+++ web/app/themes/theme/front-page.php
@@ -16,17 +16,19 @@
           <h1 class="o-site-logo c-site-logo-alt--home-intro u-icon u-icon-logo-alt-negative">
             <?php the_field('homepage_logo_title', 'option') ?>
           </h1>
-          <?php if(get_field('homepage_first_button_label', 'option')) :?>
-            <a class="o-btn c-btn--large c-btn--mint u-margin-bottom--mobile" href="<?php the_field('homepage_first_button_link', 'option') ?>">
-              <?php the_field('homepage_first_button_label', 'option') ?>
-            </a>
-          <?php endif; ?>
-          <?php if(get_field('homepage_second_button_label', 'option')) :?>
-            <a class="o-btn c-btn--large c-btn--indigo u-margin-left--tablet" href="<?php the_field('homepage_second_button_link', 'option') ?>">
-              <?php the_field('homepage_second_button_label', 'option') ?>
-            </a>
-          <?php endif; ?>
 
+          <div class="c-home-intro__buttons-wrap">
+            <?php if(get_field('homepage_first_button_label', 'option')) :?>
+              <a class="o-btn c-btn--large c-btn--mint u-margin--home-intro-button" href="<?php the_field('homepage_first_button_link', 'option') ?>">
+                <?php the_field('homepage_first_button_label', 'option') ?>
+              </a>
+            <?php endif; ?>
+            <?php if(get_field('homepage_second_button_label', 'option')) :?>
+              <a class="o-btn c-btn--large c-btn--indigo" href="<?php the_field('homepage_second_button_link', 'option') ?>">
+                <?php the_field('homepage_second_button_label', 'option') ?>
+              </a>
+            <?php endif; ?>
+          </div>
         </div>
       </div>
     </div>
