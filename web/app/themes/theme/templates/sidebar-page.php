c81b693ad23bfb4345b729b9de1d72913cc44471
Ruslan Komjakov <ruslan.komjakov@gmail.com>
2015-05-03 20:30:51 +0200

    Add page sidbar template and custom fields

---
+++ web/app/themes/theme/templates/sidebar-page.php
@@ -0,0 +1,16 @@
+<div class="c-page__sidebar">
+  <div class="c-sidebar__sidenote">
+
+  <?php if(get_field('page_sidenote_title')) :?>
+    <h2 class="o-heading c-heading--page-sidebar-note c-heading--page-sidebar-note--mint">
+      <?php the_field('page_sidenote_title') ?>
+    </h2>
+  <?php endif; ?>
+
+  <?php if(get_field('page_sidenote_text')) :?>
+    <p class="c-paragraph--sidebar c-paragraph--sidebar--grey">
+      <?php the_field('page_sidenote_text') ?>
+    </p>
+  <?php endif; ?>
+
+</div>
