07646ec6870aaa83959874b1b6774e25b3562518
Ruslan Komjakov <ruslan.komjakov@gmail.com>
2015-05-03 20:32:23 +0200

    Add page template markup and styles. Remove unnecessary templates

--- web/app/themes/theme/page.php
+++ web/app/themes/theme/page.php
@@ -1,4 +1,9 @@
-<?php while (have_posts()) : the_post(); ?>
-  <?php get_template_part('templates/page', 'header'); ?>
-  <?php get_template_part('templates/content', 'page'); ?>
-<?php endwhile; ?>
+<main class="o-wrapper">
+  <div class="o-wrapper--row">
+    <?php while (have_posts()) : the_post(); ?>
+      <h1 class="o-heading c-heading--page-title"><?php the_field('subtitle'); ?></h1>
+      <?php get_template_part('templates/content', 'page'); ?>
+      <?php get_template_part('templates/sidebar', 'page'); ?>
+    <?php endwhile; ?>
+  </div>
+</main>
