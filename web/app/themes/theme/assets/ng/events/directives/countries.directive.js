de2ed9041fe6bd9f6f1d499a7564e9d25e2ac75c
Sergei Sapoznikov <sergei.sapoznikov@gmail.com>
2015-05-04 15:16:29 +0100

    Set up countries switcher styles for the timeline

--- web/app/themes/theme/assets/ng/events/directives/countries.directive.js
+++ web/app/themes/theme/assets/ng/events/directives/countries.directive.js
@@ -7,10 +7,10 @@ angular
       template: '<div class="countries-switcher">'+
                 '<h2 class="o-btn c-btn--mint c-btn--selector" ng-click="ctrl.toggle()">' +
                     '{{ timeline.name }}' + 
-                    '<span class="u-icon u-icon-arrow-down"></span>'+
+                    '<span class="u-icon u-icon-arrow-down-negative countries-switcher__icon"></span>'+
                 '</h2>'+
                 '<ul class="countries-switcher__list" ng-show="ctrl.open">'+
-                '<li ng-repeat="country in countries" ng-click="switchCountry(country.slug)">{{ country.name }}</li>'+
+                '<li class="o-btn c-btn--mint c-btn--small" ng-repeat="country in countries" ng-click="switchCountry(country.slug)">{{ country.name }}</li>'+
                 '</ul>'+
                 '</div>',
       controller: function ($scope, $element) {
