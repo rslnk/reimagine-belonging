1385776ff4c7b18aaa85a9fde37cde513c34f5b9
Ruslan Komjakov <ruslan.komjakov@gmail.com>
2015-06-08 23:34:05 +0200

    Update story preview styles, add hero city

--- web/app/themes/theme/assets/ng/stories/directives/preview.directive.js
+++ web/app/themes/theme/assets/ng/stories/directives/preview.directive.js
@@ -5,12 +5,16 @@ angular
       restrict: 'E',
       replace: true,
       template: '<div class="story-preview__item" style="background-image: url({{story.preview_image}})">'+
-                  '<div class="color-filter" style="color: {{ color[story.color]}};"></div>'+
+                  '<div class="story-preview__image-overlay story-preview__image-overlay--gradient" style="color: {{ color[story.color]}};"></div>' +
+                  '<div class="story-preview__image-overlay story-preview__image-overlay--solid"></div>' +
                   '<div class="story-preview__content">'+
-                    '<h2 class="o-heading c-heading--story-preview">{{ story.title }}</h2>' +
-                    '<div class="hero-block">'+
-                      '<span class="o-heading c-heading--story-preview">{{ story.hero }}</span>' +
+                    '<div class="story-title">' +
+                      '<h2 class="o-heading c-heading--story-preview">{{ story.title }}</h2>' +
+                    '</div>' +
+                    '<div class="story-hero">'+
+                      '<h3><strong>{{ story.hero }}</strong>, {{ story.city[0].term_name }}</h3>' +
                     '</div>'+
+                    '<div class="post-format-icon u-icon u-icon-play-negative"></div>' +
                   '</div>'+
                 '</div>',
       link: function (scope, element) {
