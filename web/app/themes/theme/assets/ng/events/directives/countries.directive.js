26c07301ae719b582be7bfafb9ced1cffc6ee708
Ruslan Komjakov <ruslan.komjakov@gmail.com>
2015-09-19 17:27:55 +0300

    Fix filter class names
    - Gave filter class to more generic name
    - Increased the size of the drop down in the timelines selector

--- web/app/themes/theme/assets/ng/events/directives/countries.directive.js
+++ web/app/themes/theme/assets/ng/events/directives/countries.directive.js
@@ -5,12 +5,12 @@ angular
       restrict: 'E',
       replace: true,
       template: '<div class="countries-switcher">'+
-                '<h2 class="o-btn c-btn--mint c-btn--selector" ng-click="ctrl.toggle()">' +
+                '<h2 class="o-btn c-btn--selector c-btn--mint" ng-click="ctrl.toggle()">' +
                     '{{ timeline.name }}' +
                     '<span class="u-icon u-icon-arrow-down-negative countries-switcher__icon"></span>'+
                 '</h2>'+
                 '<ul class="countries-switcher__list" ng-show="ctrl.open">'+
-                '<li class="o-btn c-btn--mint c-btn--small" ng-repeat="country in siteConfig.timelines" ng-click="switchCountry(country.slug)">{{ country.name }}</li>'+
+                '<li class="o-btn c-btn--medium c-btn--mint" ng-repeat="country in siteConfig.timelines" ng-click="switchCountry(country.slug)">{{ country.name }}</li>'+
                 '</ul>'+
                 '</div>',
       controller: function ($scope, $element) {
