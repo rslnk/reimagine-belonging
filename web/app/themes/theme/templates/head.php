65023ad2f829785154a663858ace8c404605722b
Ruslan Komjakov <ruslan.komjakov@gmail.com>
2015-09-19 21:06:56 +0300

    Add meta to the site head
    - Added og meta tags
    - Added twitter large image card meta tags
    - Added `meta` settings in WP admin uneder `Settings` menu
    - Updated site languages options in `Settings` to use en_US format
    - Added alternative language to the site settings

--- web/app/themes/theme/templates/head.php
+++ web/app/themes/theme/templates/head.php
@@ -1,9 +1,6 @@
 <!doctype html>
 <html class="no-js" <?php language_attributes(); ?>>
   <head>
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="alternate" type="application/rss+xml" title="<?= get_bloginfo('name'); ?> Feed" href="<?= esc_url(get_feed_link()); ?>">
     <?php wp_head(); ?>
     <?php if (is_page_template('template-timeline.php') || is_page_template('template-stories.php')) {
