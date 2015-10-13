1fe495e574fbb876483a566415194a445ad219b4
Ruslan Komjakov <ruslan.komjakov@gmail.com>
2015-10-13 18:12:53 +0200

    Add language/domain switcher to footer
    - Add language/domain switcher fields to WordPress admin
    - Output language/domain switcher to footer
    - Move footer social icon class to `footer.styl`
    - Remove sidebar sidenotes fields from `Home` page template in WordPress admin

--- web/app/themes/theme/templates/footer.php
+++ web/app/themes/theme/templates/footer.php
@@ -8,14 +8,19 @@
         wp_nav_menu(['theme_location' => 'secondary_navigation', 'walker' => new NavWalker(), 'menu_class' => 'o-site-nav__list c-site-nav__list--footer']);
       endif;
       ?>
-      <ul class="o-social-icons__list c-social-icons__list--site-footer">
-        <li class="o-social-icons__item o-social-icons__item--site-footer">
-          <a class="o-icons-list__link c-icons-list__link--site-footer u-icon u-icon-facebook-negative" href="https://www.facebook.com/withWINGSandROOTS">Facebook</a>
+      <ul class="c-site-footer__social-icons">
+        <li>
+          <a class="o-icons-list__link u-icon u-icon-facebook-negative" href="https://www.facebook.com/withWINGSandROOTS">Facebook</a>
         </li>
-        <li class="o-social-icons__item o-social-icons__item--site-footer">
-          <a class="o-icons-list__link c-icons-list__link--site-footer u-icon u-icon-twitter-negative" href="https://www.twitter.com/wingsrootsfilm">Twitter</a>
+        <li>
+          <a class="o-icons-list__link u-icon u-icon-twitter-negative" href="https://www.twitter.com/wingsrootsfilm">Twitter</a>
         </li>
       </ul>
+      <?php if(get_field('footer_language_switcher_text', 'option') && get_field('footer_language_switcher_url', 'option')): ?>
+        <div class="c-site-footer__language-switcher">
+          <a href="<?php the_field('footer_language_switcher_url', 'option'); ?>"><?php the_field('footer_language_switcher_text', 'option'); ?></a>
+        </div>
+      <?php endif; ?>
     </nav>
   </div>
 </footer>
