005b71baaabf9e4133f40d055bc2de4815f7206b
Ruslan Komjakov <ruslan.komjakov@gmail.com>
2016-01-12 13:14:22 -0800

    Disable WordPress post revisions

--- config/application.php
+++ config/application.php
@@ -73,6 +73,7 @@ define('AS3CF_REGION', getenv('AS3CF_REGION'));
 define('AUTOMATIC_UPDATER_DISABLED', true);
 define('DISABLE_WP_CRON', getenv('DISABLE_WP_CRON') ?: false);
 define('DISALLOW_FILE_EDIT', true);
+define('WP_POST_REVISIONS', false);
 define('WP_DEFAULT_THEME', 'rebe');
 
 /**
